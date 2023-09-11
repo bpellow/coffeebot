1e3e80adfea11a6dd4624401c50bfaef2d60652e
bpellow <63289868+bpellow@users.noreply.github.com>
2023-09-11 18:34:16 +0100

    removed hardcoded user/pass

--- csv_generator.py
+++ csv_generator.py
@@ -5,10 +5,15 @@ logins = [["folder", "favorite", "type", "name", "notes", "fields",
 
 data = []
 
+# Enter user and password
+user_prefix = "willp"
+password = "scotland1"
+email_domain = "coldmail.com"
+
 
 def generate_entry(day, month):
     return ["Coffee", None, "login", "Gails", f"Date: {day:02d}/{month:02d}", f"Date: {day:02d}/{month:02d}",
-            None,  None, f"willp{day:02d}{month:02d}", "scotland1", None]
+            None,  None, f"{user_prefix}{day:02d}{month:02d}", password, None]
 
 
 thirty_day_months = [4, 6, 9, 11]
@@ -17,18 +22,18 @@ for month in range(1, 13):
     if month == 2:
         for day in range(1, 29):
             logins.append(generate_entry(day, month))
-            data.append([f"willp{day:02d}{month:02d}",
-                        f"willp{day:02d}{month:02d}@coldmail.com", day, month, 2002, "scotland1"])
+            data.append([f"{user_prefix}{day:02d}{month:02d}",
+                        f"{user_prefix}{day:02d}{month:02d}@{email_domain}", day, month, 2002, password])
     elif month in thirty_day_months:
         for day in range(1, 31):
             logins.append(generate_entry(day, month))
-            data.append([f"willp{day:02d}{month:02d}",
-                        f"willp{day:02d}{month:02d}@coldmail.com", day, month, 2002, "scotland1"])
+            data.append([f"{user_prefix}{day:02d}{month:02d}",
+                        f"{user_prefix}{day:02d}{month:02d}@{email_domain}", day, month, 2002, password])
     else:
         for day in range(1, 32):
             logins.append(generate_entry(day, month))
-            data.append([f"willp{day:02d}{month:02d}",
-                        f"willp{day:02d}{month:02d}@coldmail.com", day, month, 2002, "scotland1"])
+            data.append([f"{user_prefix}{day:02d}{month:02d}",
+                        f"{user_prefix}{day:02d}{month:02d}@{email_domain}", day, month, 2002, password])
 
 
 with open("logins.csv", "w", newline="") as file:
--- csv_generator_current.py
+++ csv_generator_current.py
@@ -5,10 +5,15 @@ logins = [["folder", "favorite", "type", "name", "notes", "fields",
 
 data = []
 
+# Enter user and password:
+user_prefix = "willp"
+password = "scotland1"
+domain = "coldmail.com"
+
 
 def generate_entry(day, month):
     return ["Coffee", None, "login", "Gails", f"Date: {day:02d}/{month:02d}", f"Date: {day:02d}/{month:02d}",
-            None,  None, f"avigz{day:02d}{month:02d}", "coldpassword", None]
+            None,  None, f"{user_prefix}{day:02d}{month:02d}", password, None]
 
 
 current_day = 11
@@ -16,8 +21,8 @@ month = 9
 days_in_future = 14
 for day in range(current_day, current_day + days_in_future + 1):
     logins.append(generate_entry(day, month))
-    data.append([f"avigz{day:02d}{month:02d}",
-                 f"avigz{day:02d}{month:02d}@coldmail.com", day, month, 2002, "coldpassword"])
+    data.append([f"{user_prefix}{day:02d}{month:02d}",
+                 f"{user_prefix}{day:02d}{month:02d}@{domain}", day, month, 2002, password])
 
 
 with open("logins_current.csv", "w", newline="") as file:
